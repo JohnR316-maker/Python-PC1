c2553f954312466df909be91d5e57a62dac9b07b
JohnR316-maker <prolightjohncito316@gmail.com>
2024-07-14 19:21:10 +0000

    Subiendo Problema5

---
+++ Problema5.py
@@ -0,0 +1,9 @@
+#5) Escribir un programa que solicite al usuario que ingrese cuántos shows musicales ha visto en el último año y almacene
+# ese número en una variable. A continuación, mostrar en pantalla un valor de verdad (True o False - tipo de datos booleanos) 
+# que indique si el usuario ha visto más de 3 shows.
+
+Shows_vistos_en_el_último_año = 6
+
+a = 3
+
+Shows_vistos_en_el_último_año > a
